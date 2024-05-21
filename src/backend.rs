9be2df19698aacebd15d9163b1d80ebe33e96847
fwcd <fwcd@fwcd.dev>
2024-05-22 01:00:20 +0200

    Set up basic language server

---
+++ src/backend.rs
@@ -0,0 +1,29 @@
+use tower_lsp::{jsonrpc::Result, lsp_types::{InitializeParams, InitializeResult, InitializedParams, MessageType}, Client, LanguageServer};
+
+#[derive(Debug)]
+pub struct Backend {
+    client: Client,
+}
+
+impl Backend {
+    pub fn new(client: Client) -> Self {
+        Self { client }
+    }
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult::default())
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "Server initialized!")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod backend;
+
+use backend::Backend;
+use clap::Parser;
+use tower_lsp::{LspService, Server};
+
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    // TODO: Add host/port for running as TCP server or client
+}
+
+#[tokio::main]
+async fn main() {
+    let _args = Args::parse();
+
+    let in_stream = tokio::io::stdin();
+    let out_stream = tokio::io::stdout();
+
+    let (service, socket) = LspService::new(|client| Backend::new(client));
+    Server::new(in_stream, out_stream, socket).serve(service).await;
 }
